00b084a2ce70e46a055618a9ed3b7a016c111c88
Cheng Chang <chc012@stanford.edu>
2023-12-05 02:33:11 -0800

    edit before first train

--- train.py
+++ train.py
@@ -42,10 +42,12 @@ def main():
         learning_rate=2e-5,
         per_device_train_batch_size=16,
         per_device_eval_batch_size=16,
-        num_train_epochs=2,
+        num_train_epochs=5,
         weight_decay=0.01,
         evaluation_strategy="epoch",
         save_strategy="epoch",
+        logging_dir="logs",
+        logging_steps=100,
         load_best_model_at_end=True
     )
 
